7e206fb996a7875f267d73ed46db336e25e9e3d6
Vallory <carlvallory@gmail.com>
2023-07-05 16:26:26 -0400

    App Expect Logout 1.0

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const qrcode = require('qrcode-terminal');
 const axios = require('axios');
+const http = require('http');
+const url = require('url');
 const { Client, NoAuth } = require('whatsapp-web.js');
 const nextBase64 = require('next-base64');
 
-const API_KEY = "";
+const API_KEY = process.env.API_KEY || "";
 const RECEIVER_URL = process.env.WA_RECEIVER_URL;
 const RECEIVER_PATH = process.env.WA_RECEIVER_PATH;
+const HOSTNAME = process.env.HTTP_HOSTNAME;
+const PORT = process.env.HTTP_PORT || 3000;
 
-const laramsgURL = "http://phplaravel-937280-3593957.cloudwaysapps.com/msg";
+const laramsgURL = "http://phplaravel-1040427-3658816.cloudwaysapps.com/msg";
 
 const client = new Client(
     {
@@ -22,13 +26,20 @@ let msgObj = {
     updated: false,
     msg: {
         id: null,
-        body: "",
+        body: {
+            text: "",
+            image: null
+        },
+        image: null,
         to: {
-            id: null
+            id: null,
+            name: null,
+            user: null
         },
         from: {
             id: null,
             name: null,
+            user: null,
             image: null
         },
         profile: {
@@ -55,14 +66,21 @@ client.on('auth_failure', msg => {
     console.error('AUTHENTICATION FAILURE', msg);
 });
 
-client.on('ready', () => {
+client.on('ready', async () => {
+    msgObj.msg.to.id    = client.info.wid.user;
+    msgObj.msg.to.user  = client.info.wid.user;
+    msgObj.msg.to.name  = client.info.wid.name;
+
+    let getStatus = await getSendLogin(String(msgObj.msg.id));
+
+    console.log(client.info.wid.user);
     console.log('Client is Ready');
 });
 
 client.on('message', async msg => {
     const isBroadcast = msg.broadcast || msg.isStatus;
 
-    if(msg.type != "sticker" && msg.type != "video" && msg.type != "image"){ //permitir imagenes
+    if(msg.type != "sticker" && msg.type != "video"){ //permitir imagenes // && msg.type != "image"
         if(msg.hasMedia == false){
             //type chat
             if(msg.type == "chat"){
@@ -70,10 +88,13 @@ client.on('message', async msg => {
                 const profilePicture = await contact.getProfilePicUrl();
 
                 msgObj.msg.id           = msg.id.id;
-                msgObj.msg.body         = nextBase64.encode(String(msg.body));
+                msgObj.msg.body.text    = nextBase64.encode(String(msg.body));
                 msgObj.msg.to.id        = msg.to;
                 msgObj.msg.from.id      = msg.from;
 
+                if(msg.type=="image") {
+
+                }
                 if(msg._data.notifyName !== undefined) { 
                     msgObj.msg.from.name = nextBase64.encode(String(msg._data.notifyName));
                 } else {
@@ -90,12 +111,12 @@ client.on('message', async msg => {
                 let mfni = 1;
                 let mppi = 1;
 
-                while(msgObj.msg.body.includes("/") === true) {
+                while(msgObj.msg.body.text.includes("/") === true) {
                     mbi++;
-                    msgObj.msg.body = nextBase64.encode(String(msgObj.msg.body));
+                    msgObj.msg.body.text = nextBase64.encode(String(msgObj.msg.body.text));
                 }
 
-                msgObj.msg.body = msgObj.msg.body + "_" + mbi;
+                msgObj.msg.body.text = msgObj.msg.body.text + "_" + mbi;
 
                 while(msgObj.msg.from.name.includes("/") === true) {
                     mfni++;
@@ -120,10 +141,11 @@ client.on('message', async msg => {
                 
                 console.log('ID: ', msg.id.id);
                 console.log('MESSAGE RECEIVED', msg.body);
-                //console.log(msg);
+                console.log(msg);
 
                 if(isBroadcast == false) {
-                    let getMsg = await getSendMsg(msg.id.id, msgObj.msg.body, msgObj);
+                    let getMsg = await getSendMsg(msg.id.id, msgObj.msg.body.text, msgObj);
+                    let getStatus = await getSendStatus(String(msgObj.msg.id));
                     console.log(getMsg);
                 }
             }
@@ -131,12 +153,17 @@ client.on('message', async msg => {
     }
 });
 
+client.on('disconnected', (reason) => {
+    console.log('Client is disconected: ', reason);
+    client.initialize();
+});
+
 client.initialize();
 
 (async() => {
     msgObj.msg.id               = 1;
-    msgObj.msg.body             = nextBase64.encode("Muy Buenos Días!!!");
-    msgObj.msg.body             = msgObj.msg.body + "_1";
+    msgObj.msg.body.text             = nextBase64.encode("Muy Buenos Días!!!");
+    msgObj.msg.body.text             = msgObj.msg.body.text + "_1";
     msgObj.msg.to.id            = 10;
     msgObj.msg.from.id          = 11;
     msgObj.msg.from.name        = nextBase64.encode("name");
@@ -145,7 +172,9 @@ client.initialize();
     msgObj.msg.participant      = false;
     msgObj.msg.profile.picture  = null;
 
-    let getMsg = await getSendMsg(msgObj.msg.id, msgObj.msg.body, msgObj);
+    let getMsg = await getSendMsg(msgObj.msg.id, msgObj.msg.body.text, msgObj);
+    let getStatus = await getSendStatus(String(msgObj.msg.id));
+    
     console.log(getMsg);
   })()
 
@@ -211,5 +240,87 @@ async function getSendQR(qr) {
         return data;
     } catch (error) {
         console.error(error.response);
+    }
+}
+
+async function getSendLogin(user) {
+    encodedUser = nextBase64.encode(user);
+    let url = "/login/"+encodedUser; 
+
+    const laramsgApi = axios.create({
+        baseURL: laramsgURL,
+        params:
+        {
+            key: API_KEY
+        },
+    });
+
+    console.warn(laramsgURL);
+    console.warn(url);
+
+    try {
+        const { data } = await laramsgApi.get(url);
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error.response);
+    }
+}
+
+async function getSendStatus(id) {
+    encodedId = nextBase64.encode(id);
+    let url = "/schedules/"+encodedId; 
+
+    const laramsgApi = axios.create({
+        baseURL: laramsgURL,
+        params:
+        {
+            key: API_KEY
+        },
+    });
+
+    console.warn(laramsgURL);
+    console.warn(url);
+
+    try {
+        const { data } = await laramsgApi.get(url);
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.error(error.response);
+    }
+}
+
+process.on('unhandledRejection', (error) => {
+    console.error('Unhandled Promise Rejection:', error);
+});
+
+const server = http.createServer((req, res) => {
+    const baseURL =  req.protocol + '://' + req.headers.host + '/';
+    const reqUrl = new URL(req.url,baseURL);
+
+    if(reqUrl.pathname == "/logout") {
+        client.getState().then((result) => {
+            if(result.match("CONNECTED")){
+                var q = url.parse(req.url, true).query;
+                var user = q.user;
+                var logout = setLogout(user);
+            } else {
+                console.error("Whatsapp Client not connected")
+            }
+        });
+    }
+    
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end();
+}).listen(PORT); 
+
+function setLogout(user) { 
+    try {
+        client.logout();
+        return true;
+    } catch (error) {
+        console.error(error.response);
+        return false;
     }
 }
